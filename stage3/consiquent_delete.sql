839af65a37195b3bbc9f49a63d94f43b65d2c122
artemiyjjj <artemiyromanoff@gmail.com>
2023-11-05 23:51:47 +0300

    Added stage 3 files: database entities create and delete scripts

---
+++ stage3/consiquent_delete.sql
@@ -0,0 +1,14 @@
+DROP FUNCTION IF EXISTS count_group_countries(group_id integer) CASCADE;
+
+DROP FUNCTION IF EXISTS count_event_groups(groups_id integer) CASCADE;
+
+DROP TABLE IF EXISTS country CASCADE;
+
+DROP TABLE IF EXISTS event_groups CASCADE;
+
+DROP TABLE IF EXISTS event_group CASCADE;
+
+DROP TABLE IF EXISTS political_status CASCADE;
+
+DROP TABLE IF EXISTS country_relationship_event_history CASCADE;
+

599ab5f15bbb72024be026549989fe9f4f2932c2
artemiyjjj <artemiyromanoff@gmail.com>
2023-11-06 11:04:23 +0300

    Updated all-database-objects-deleting script

--- stage3/consiquent_delete.sql
+++ stage3/consiquent_delete.sql
@@ -1,14 +1,49 @@
-DROP FUNCTION IF EXISTS count_group_countries(group_id integer) CASCADE;
-
-DROP FUNCTION IF EXISTS count_event_groups(groups_id integer) CASCADE;
-
-DROP TABLE IF EXISTS country CASCADE;
+DROP TABLE IF EXISTS country_relationship_event_history CASCADE;
 
 DROP TABLE IF EXISTS event_groups CASCADE;
 
 DROP TABLE IF EXISTS event_group CASCADE;
 
+DROP FUNCTION IF EXISTS count_group_countries(group_id integer) CASCADE;
+
+DROP FUNCTION IF EXISTS count_event_groups(groups_id integer) CASCADE;
+
 DROP TABLE IF EXISTS political_status CASCADE;
 
-DROP TABLE IF EXISTS country_relationship_event_history CASCADE;
+DROP TABLE IF EXISTS country CASCADE;
 
+
+DROP TABLE IF EXISTS people_detachment_to_building CASCADE;
+
+DROP TABLE IF EXISTS building_construction_artefact CASCADE;
+
+DROP TABLE IF EXISTS building CASCADE;
+
+DROP TABLE IF EXISTS building_type CASCADE;
+
+DROP TABLE IF EXISTS report CASCADE;
+
+
+DROP TABLE IF EXISTS person_position_history CASCADE;
+
+DROP TABLE IF EXISTS position CASCADE;
+
+DROP TABLE IF EXISTS Person CASCADE;
+
+
+DROP TABLE IF EXISTS family_resource_ownership CASCADE;
+
+DROP TABLE IF EXISTS family CASCADE;
+
+DROP TABLE IF EXISTS craft_type CASCADE;
+
+
+DROP TABLE IF EXISTS resource_usage CASCADE;
+
+DROP TABLE IF EXISTS resource_usage_type CASCADE;
+
+DROP TABLE IF EXISTS resource CASCADE;
+
+DROP TABLE IF EXISTS resource_storage CASCADE;
+
+DROP TABLE IF EXISTS resource_type CASCADE;
